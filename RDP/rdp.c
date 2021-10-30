3645ad66d558f852b51c02e6f42cddbfe3e935b7
Akshith S <63113499+AkshithS6063@users.noreply.github.com>
2021-10-30 22:35:17 +0530

    error checking corrections.

--- RDP/rdp.c
+++ RDP/rdp.c
@@ -1,5 +1,5 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
 char str[20];
 int x = 0;
@@ -9,58 +9,85 @@ void F();
 void E1();
 void T1();
 
-void F(){
-	printf("F\n");
-	if(str[x] == '(' ){
-		x++;
-	}
-	if(str[x] == ')'){
-		x++;
-	}
-	if(str[x] == 'i'){
-		x++;
-	}
+void F()
+{
+    printf("F\n");
+    if (str[x] == '(')
+    {
+        x++;
+    }
+    if (str[x] == ')')
+    {
+        x++;
+    }
+    if (str[x] == 'i')
+    {
+        x++;
+    }
 }
 
-void T1(){
-	printf("T1\n");
-	if(str[x] == '*' ) {
-		x++;
-		F();
-		T1();
-	}
+void T1()
+{
+    printf("T1\n");
+    if (str[x] == '*')
+    {
+        x++;
+        if (str[x] == 'i')
+        {
+            F();
+            T1();
+        }
+        else{
+            printf("Invalid input");
+        }
+    }
 }
 
-void T(){
-	printf("T\n");
-	F();
-	T1();
-} 
-
-void E1(){
-	if(str[x] == '+' ) {
-		x++;
-		T();
-	}
+void T()
+{
+    printf("T\n");
+    F();
+    T1();
 }
 
-void E(){
-	printf("E\n");
-	T();
-	E1();
+void E1()
+{
+    if (str[x] == '+')
+    {
+        x++;
+        if (str[x] == 'i')
+        {
+            T();
+        }
+        else
+        {
+            printf("Input invalid \n");
+            x--;
+        }
+    }
 }
 
-void main(){
-	
-	printf("input\n");
-	fgets(str,20,stdin);
-	int strl = strlen(str)-1;
-	E();
-	printf("%d", x);
-	if(x == strl){
-		printf("A\n");
-	}
-	else{
-		printf("R\n");
-	}
+void E()
+{
+    printf("E\n");
+    T();
+    E1();
+}
+
+void main()
+{
+
+    printf("input\n");
+    fgets(str, 20, stdin);
+    int strl = strlen(str) - 1;
+    E();
+    printf("%d", x);
+    if (str[0] == str[strl-1])
+    {
+        printf("A\n");
+    }
+    else
+    {
+        printf("R\n");
+    }
 }
